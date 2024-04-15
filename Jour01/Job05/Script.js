120440198bc6337cac00eec512405b4efcbd1f5e
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2024-04-15 11:24:05 +0200

    Job04,05,06 finis

---
+++ Jour01/Job05/Script.js
@@ -0,0 +1,11 @@
+function afficherJoursSemaines() {
+    var joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    console.log("Les jours de la semaine sont :");
+    for (var i = 0; i < joursSemaines.length; i++) {
+        console.log(joursSemaines[i]);
+    }
+}
+
+
+afficherJoursSemaines();
